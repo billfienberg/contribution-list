6603b93c17033812ca41818c2c73190597e294ce
Bill Fienberg <billfienberg@gmail.com>
2020-04-10 15:14:34 -0500

    Add mockData to use in tests

---
+++ src/mockData.js
@@ -0,0 +1,207 @@
+const data = {
+  data: {
+    user: {
+      repositoriesContributedTo: {
+        totalCount: 16,
+        nodes: [
+          {
+            id: "MDEwOlJlcG9zaXRvcnkzMzIwMjY2Nw==",
+            owner: {
+              id: "MDEyOk9yZ2FuaXphdGlvbjU0MjE1NjM=",
+              login: "department-of-veterans-affairs",
+            },
+            name: "vets-website",
+            description: "Frontend for VA.gov",
+            stargazers: {
+              totalCount: 158,
+            },
+          },
+          {
+            id: "MDEwOlJlcG9zaXRvcnk1NjIzNzYxMg==",
+            owner: {
+              id: "MDEyOk9yZ2FuaXphdGlvbjIwMzQ0NTg=",
+              login: "algolia",
+            },
+            name: "docsearch-configs",
+            description: "DocSearch - Configurations",
+            stargazers: {
+              totalCount: 238,
+            },
+          },
+          {
+            id: "MDEwOlJlcG9zaXRvcnk5MzU2NTU4Mg==",
+            owner: {
+              id: "MDEyOk9yZ2FuaXphdGlvbjI5MjM5NDQ3",
+              login: "howtographql",
+            },
+            name: "howtographql",
+            description: "The Fullstack Tutorial for GraphQL",
+            stargazers: {
+              totalCount: 6529,
+            },
+          },
+          {
+            id: "MDEwOlJlcG9zaXRvcnk5NDIyNjQ0Mg==",
+            owner: {
+              id: "MDEyOk9yZ2FuaXphdGlvbjI5MjM5NDQ3",
+              login: "howtographql",
+            },
+            name: "graphql-ruby",
+            description: "GraphQL Ruby example for How To GraphQL",
+            stargazers: {
+              totalCount: 207,
+            },
+          },
+          {
+            id: "MDEwOlJlcG9zaXRvcnk5NDM2NzY3Nw==",
+            owner: {
+              id: "MDQ6VXNlcjQwNjAxODc=",
+              login: "jaredpalmer",
+            },
+            name: "formik",
+            description: "Build forms in React, without the tears 😭 ",
+            stargazers: {
+              totalCount: 21449,
+            },
+          },
+          {
+            id: "MDEwOlJlcG9zaXRvcnkxMzM4NDMxMjU=",
+            owner: {
+              id: "MDEyOk9yZ2FuaXphdGlvbjU0MjE1NjM=",
+              login: "department-of-veterans-affairs",
+            },
+            name: "va.gov-team",
+            description: null,
+            stargazers: {
+              totalCount: 48,
+            },
+          },
+          {
+            id: "MDEwOlJlcG9zaXRvcnkxNDU4NzE2OTI=",
+            owner: {
+              id: "MDEyOk9yZ2FuaXphdGlvbjU0MjE1NjM=",
+              login: "department-of-veterans-affairs",
+            },
+            name: "va.gov-vfs-teams",
+            description: "Documentation for External Contractor teams working on the Veteran-facing Services Platform",
+            stargazers: {
+              totalCount: 7,
+            },
+          },
+          {
+            id: "MDEwOlJlcG9zaXRvcnkxNDkxMzk0ODk=",
+            owner: {
+              id: "MDEyOk9yZ2FuaXphdGlvbjUyNzI0OTQ=",
+              login: "youearnedit",
+            },
+            name: "YEI-lunchnlearn",
+            description: " A place for tracking things we talk about, or want to talk about, at Lunch 'n' Learn.",
+            stargazers: {
+              totalCount: 2,
+            },
+          },
+          {
+            id: "MDEwOlJlcG9zaXRvcnkxNTI2NjQyNDI=",
+            owner: {
+              id: "MDEyOk9yZ2FuaXphdGlvbjU0MjE1NjM=",
+              login: "department-of-veterans-affairs",
+            },
+            name: "vets-design-system-documentation",
+            description: "Repository for design.va.gov website",
+            stargazers: {
+              totalCount: 8,
+            },
+          },
+          {
+            id: "MDEwOlJlcG9zaXRvcnkxNjY0ODE1NDg=",
+            owner: {
+              id: "MDQ6VXNlcjE4MjExMjM=",
+              login: "TimothyClayton",
+            },
+            name: "graphql-react-client",
+            description:
+              "This is the React part of a demo for GraphQL subscriptions with a standalone Rails API and a React App frontend.",
+            stargazers: {
+              totalCount: 2,
+            },
+          },
+          {
+            id: "MDEwOlJlcG9zaXRvcnkxNzE2OTU2NjM=",
+            owner: {
+              id: "MDEyOk9yZ2FuaXphdGlvbjU0MjE1NjM=",
+              login: "department-of-veterans-affairs",
+            },
+            name: "veteran-facing-services-tools",
+            description:
+              "Packages for formation.js (design.va.gov) and the VA.gov Client Documentation Site (https://department-of-veterans-affairs.github.io/veteran-facing-services-tools/getting-started/internal-tools/)",
+            stargazers: {
+              totalCount: 12,
+            },
+          },
+          {
+            id: "MDEwOlJlcG9zaXRvcnkxOTYyMjI2OTk=",
+            owner: {
+              id: "MDEyOk9yZ2FuaXphdGlvbjQ1NDMzNDY2",
+              login: "KazooHR",
+            },
+            name: "engineering-lunchnlearn",
+            description: "A place for tracking things we talk about, or want to talk about, at Lunch 'n' Learn.",
+            stargazers: {
+              totalCount: 1,
+            },
+          },
+          {
+            id: "MDEwOlJlcG9zaXRvcnkyMjY0MTYyMjA=",
+            owner: {
+              id: "MDQ6VXNlcjE3NjAxMw==",
+              login: "wesbos",
+            },
+            name: "advanced-react-rerecord",
+            description: "Trying things out. Feel free to follow along",
+            stargazers: {
+              totalCount: 134,
+            },
+          },
+          {
+            id: "MDEwOlJlcG9zaXRvcnkyMjkyODAzMzU=",
+            owner: {
+              id: "MDEyOk9yZ2FuaXphdGlvbjU5MDkxNzMx",
+              login: "bulkstory",
+            },
+            name: "web",
+            description: null,
+            stargazers: {
+              totalCount: 0,
+            },
+          },
+          {
+            id: "MDEwOlJlcG9zaXRvcnkyMjkyODA1MTk=",
+            owner: {
+              id: "MDEyOk9yZ2FuaXphdGlvbjU5MDkxNzMx",
+              login: "bulkstory",
+            },
+            name: "api",
+            description: null,
+            stargazers: {
+              totalCount: 0,
+            },
+          },
+          {
+            id: "MDEwOlJlcG9zaXRvcnkyMjkyODA1NTQ=",
+            owner: {
+              id: "MDEyOk9yZ2FuaXphdGlvbjU5MDkxNzMx",
+              login: "bulkstory",
+            },
+            name: "mobile",
+            description: null,
+            stargazers: {
+              totalCount: 0,
+            },
+          },
+        ],
+      },
+    },
+  },
+}
+
+export default data
