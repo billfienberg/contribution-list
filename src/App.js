23cf5393593eb5615340b6db941edfb4d810838d
Bill Fienberg <billfienberg@gmail.com>
2020-04-10 15:25:13 -0500

    Extract Intro component from App.js

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from "react"
 import { fetchReposContributedToByUser } from "./api"
+import Intro from "./Intro"
 
 class App extends React.Component {
   constructor(props) {
@@ -33,12 +34,7 @@ class App extends React.Component {
 
     return (
       <div className="App">
-        <h1>Contribution List</h1>
-        <p>1. Type a GitHub username into the text input (for example, kentcdodds).</p>
-        <p>
-          2. Click the <b>Fetch Contributions</b> button.
-        </p>
-        <p>3. See a list of repos that user has contributed to.</p>
+        <Intro />
         <form onSubmit={onSubmit}>
           <div>
             <label>
---
+++ src/Intro.js
@@ -0,0 +1,16 @@
+import React from "react"
+
+function Intro() {
+  return (
+    <>
+      <h1>Contribution List</h1>
+      <p>1. Type a GitHub username into the text input (for example, kentcdodds).</p>
+      <p>
+        2. Click the <b>Fetch Contributions</b> button.
+      </p>
+      <p>3. See a list of repos that user has contributed to.</p>
+    </>
+  )
+}
+
+export default Intro
