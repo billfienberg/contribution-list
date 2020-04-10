bb1536cb0cade228e58cf892b7dd8d330271744d
Bill Fienberg <billfienberg@gmail.com>
2020-04-10 16:04:53 -0500

    Update index.js to use ApolloProvider

--- src/index.js
+++ src/index.js
@@ -3,9 +3,15 @@ import ReactDOM from "react-dom"
 import App from "./App"
 import "bootstrap/dist/css/bootstrap.min.css"
 
+// https://www.howtographql.com/react-apollo/1-getting-started/
+import { ApolloProvider } from "react-apollo"
+import client from "./api"
+
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <ApolloProvider client={client}>
+      <App client={client} />
+    </ApolloProvider>
   </React.StrictMode>,
   document.getElementById("root"),
 )
