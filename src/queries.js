cfd52923c06250fb22f10267c69abd79c5bc0f9c
Bill Fienberg <billfienberg@gmail.com>
2020-04-10 16:05:10 -0500

    Add graphql-tag to queries.js

--- src/queries.js
+++ src/queries.js
@@ -1,5 +1,7 @@
+import gql from "graphql-tag"
+
 // https://developer.github.com/v4/explorer/
-export const REPOSITORIES_CONTRIBUTED_TO_QUERY = `
+export const REPOSITORIES_CONTRIBUTED_TO_QUERY = gql`
   query RepositoriesContributedTo($username: String!) {
     user(login: $username) {
       repositoriesContributedTo(first: 50, privacy: PUBLIC) {
